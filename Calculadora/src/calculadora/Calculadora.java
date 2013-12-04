353b6527d45661b4cf79e66e9e5422badb610666
paooro <paorozco07@gmail.com>
2013-12-04 17:28:13 -0500

    Primer commit del proyecto calculadora.

---
+++ Calculadora/src/calculadora/Calculadora.java
@@ -0,0 +1,70 @@
+package calculadora;
+
+
+
+public class Calculadora {
+	
+	private String op;
+	private Float num1;
+	private Float num2;
+	private Float result;
+	
+	public Calculadora(){
+		
+	}
+	
+	public int sumar(float a, float b){
+		int c;
+		c= (int) (a+b) ;
+		return c;
+	}
+	
+	
+
+	public String getOp() {
+		return op;
+	}
+
+
+
+	public void setOp(String op) {
+		this.op = op;
+	}
+
+
+
+	public Float getNum1() {
+		return num1;
+	}
+
+
+
+	public void setNum1(Float num1) {
+		this.num1 = num1;
+	}
+
+
+
+	public Float getNum2() {
+		return num2;
+	}
+
+
+
+	public void setNum2(Float num2) {
+		this.num2 = num2;
+	}
+
+
+
+	public Float getResult() {
+		return result;
+	}
+
+
+
+	public void setResult(Float result) {
+		this.result = result;
+	}
+
+}
---
+++ Calculadora/src/calculadora/Main.java
@@ -0,0 +1,13 @@
+package calculadora;
+
+
+public class Main {
+	
+
+
+	public static void main(String[] args) {
+			
+		
+	}
+
+}
