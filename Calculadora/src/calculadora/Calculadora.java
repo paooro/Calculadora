1f6822b988e6f0cec716d208e90e73e65a07d1f7
miguewn06 <miguewn06 @hotmail.com>
2013-12-04 23:59:37 -0500

    Commit para el proyecto Calculadora funcionando completo.

--- Calculadora/src/calculadora/Calculadora.java
+++ Calculadora/src/calculadora/Calculadora.java
@@ -1,67 +1,68 @@
 package calculadora;
 
-
-
 public class Calculadora {
-	
+
 	private String op;
 	private Float num1;
 	private Float num2;
 	private Float result;
-	
-	public Calculadora(){
-		
+
+	public Calculadora() {
+
 	}
-	
-	public int sumar(float a, float b){
+
+	public int sumar(float a, float b) {
 		int c;
-		c= (int) (a+b) ;
+		c = (int) (a + b);
 		return c;
 	}
-	
-	
+
+	public int restar(float a, float b) {
+		int c;
+		c = (int) (a - b);
+		return c;
+	}
+
+	public float dividir(float a, float b) {
+		float c;
+
+		c = a / b;
+		return c;
+	}
+
+	public int multiplicar(float a, float b) {
+		int c;
+		c = (int) (a * b);
+		return c;
+	}
 
 	public String getOp() {
 		return op;
 	}
 
-
-
 	public void setOp(String op) {
 		this.op = op;
 	}
-
-
 
 	public Float getNum1() {
 		return num1;
 	}
 
-
-
 	public void setNum1(Float num1) {
 		this.num1 = num1;
 	}
-
-
 
 	public Float getNum2() {
 		return num2;
 	}
 
-
-
 	public void setNum2(Float num2) {
 		this.num2 = num2;
 	}
 
-
-
 	public Float getResult() {
 		return result;
 	}
-
-
 
 	public void setResult(Float result) {
 		this.result = result;
--- Calculadora/src/calculadora/Main.java
+++ Calculadora/src/calculadora/Main.java
@@ -6,57 +6,107 @@ import java.io.InputStreamReader;
 
 import calculadora.Calculadora;
 
-
-
 public class Main {
-	
 
-			private Calculadora calculadora;
-			private  int op;
-			private int num1;
-			private int num2;
-			private float result;
+	private Calculadora calculadora;
+	private static int op;
+	private int num1;
+	private int num2;
 
-			public static void main(String[] args) throws NumberFormatException, IOException {
-							
-				InputStreamReader isr = new InputStreamReader(System.in);
-				BufferedReader  br = new BufferedReader (isr);
-				
-				
-				Calculadora cal= new Calculadora();
-				
-				System.out.println("Bienvenido a su Calculadora");
-				System.out.println("Escoja la opcion que desea realizar:");
-				System.out.println("1.Sumar");
-				System.out.println("2.Restar");
-				System.out.println("3.Dividir");
-				System.out.println("4.Multiplicar");
-				System.out.println("5.Salir");
-				
-				int op = Integer.parseInt(br.readLine());
-				
-				switch (op) {
-				case 1:
-					     System.out.println("Ingrese primer número");
-					     int num1 = Integer.parseInt(br.readLine());
-					     System.out.println("Ingrese segundo número");
-					     int num2 = Integer.parseInt(br.readLine());
-					     
-					    int result= cal.sumar(num1, num2);
-					     
-					     System.out.println("El resultado de la suma es de: "+result);
-					
-					
-					break;
+	private int num3;
+	private int num4;
 
-				default:
-					break;
-				}
-				
-				
-				
-			}
+	private int num5;
+	private int num6;
 
-			
+	private int num7;
+	private int num8;
+
+	private float result1;
+	private float result2;
+	private float result3;
+	private float result4;
+
+	public static void main(String[] args) throws NumberFormatException,
+			IOException {
+
+		InputStreamReader isr = new InputStreamReader(System.in);
+		BufferedReader br = new BufferedReader(isr);
+
+		Calculadora cal = new Calculadora();
 		
+
+		do {
+
+			System.out.println("Bienvenido a su Calculadora");
+			System.out.println("Escoja la opcion que desea realizar:");
+			System.out.println("1.Sumar");
+			System.out.println("2.Restar");
+			System.out.println("3.Dividir");
+			System.out.println("4.Multiplicar");
+			System.out.println("5.Salir");
+			
+			int op = Integer.parseInt(br.readLine());
+
+			switch (op) {
+			case 1:
+				System.out.println("Ingrese primer número");
+				int num1 = Integer.parseInt(br.readLine());
+				System.out.println("Ingrese segundo número");
+				int num2 = Integer.parseInt(br.readLine());
+
+				int result1 = cal.sumar(num1, num2);
+
+				System.out.println("El resultado de la suma es de: " + result1);
+
+				break;
+			case 2:
+				System.out.println("Ingrese primer número");
+				int num3 = Integer.parseInt(br.readLine());
+				System.out.println("Ingrese segundo número");
+				int num4 = Integer.parseInt(br.readLine());
+
+				int result2 = cal.restar(num3, num4);
+
+				System.out
+						.println("El resultado de la resta es de: " + result2);
+
+				break;
+			case 3:
+				System.out.println("Ingrese primer número");
+				int num5 = Integer.parseInt(br.readLine());
+				System.out
+						.println("Ingrese segundo número (Diferente de cero)");
+				int num6 = Integer.parseInt(br.readLine());
+
+				if (num6 != 0) {
+					float result3 = cal.dividir(num5, num6);
+					System.out.println("El resultado de la división es de: "
+							+ result3);
+				} else {
+					System.out
+							.println("La operación no se puede realizar, el segundo número es cero");
+				}
+				break;
+			case 4:
+				System.out.println("Ingrese primer número");
+				int num7 = Integer.parseInt(br.readLine());
+				System.out.println("Ingrese segundo número");
+				int num8 = Integer.parseInt(br.readLine());
+
+				int result4 = cal.multiplicar(num7, num8);
+
+				System.out.println("El resultado de la multiplicación es de: "
+						+ result4);
+
+				break;
+
+			default:
+				break;
+			}
+		} while (op!= 5);
+		
+
 	}
+
+}
